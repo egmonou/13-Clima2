7ca23bb184089fdbf4f24cb47b427874f19620a6
Nour <mohamednour25@gmail.com>
2024-06-23 20:18:02 +0300

    02

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,52 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextFeild: UITextField!
+    
+    var weatherManager = WeatherManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextFeild.delegate = self
     }
-
+    
+    
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextFeild.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //print(searchTextFeild.text!)
+        searchTextFeild.endEditing(true)
+        return true
+        
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        }else {
+            textField.placeholder = "Type Something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //user search textfeild.txt to get the weather for the city
+        if let city = searchTextFeild.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        
+        searchTextFeild.text = ""
+    }
+    
+    
 
 }
 
--- Model/WeatherManager.swift
+++ Model/WeatherManager.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?&appid=71b470e7bdf63fadef56a2f4383bd91d&units=metric"
+    
+    func fetchWeather(cityName: String){
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String){
+        //1. Create a URl (get and copy the url)
+        if let url = URL(string: urlString) {
+            //2. Ceate a URL sesssion (Open the browwser)
+            let session = URLSession(configuration: .default)
+            //3. Give a sesseion task (past the URL to the browwser to fetch the data)
+            //let task = session.dataTask(with: url, completionHandler: handle(data:repsonse:error:))
+            let task = session.dataTask(with: url) { (data, repsonse, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString ?? 0 )
+                }
+            }
+            //4. Start the task (click enter)
+            task.resume()
+        }
+    }
+    
+    
+    
+}
